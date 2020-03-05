25b6bb7f70e0c34998f0565ace7349df6128cbbb
Grégoire Meunier <grigusky@MacBook-Pro-de-Gregoire.local>
2020-03-05 11:21:13 +0100

    greg wanna kill is self

--- serveur.js
+++ serveur.js
@@ -5,7 +5,14 @@ const redis = require('redis');
 const redisStore = require('connect-redis')(session);
 const client  = redis.createClient();
 const router = express.Router();
-const app = express();
+
+var http = require('http');
+var app = express();
+
+// part socket.io
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+//
 
 app.use(session({
     secret: 'secret',
@@ -40,6 +47,10 @@ router.post("/stream",(req,res) => {
     res.end('done');
 });
 
+router.get('/streaming', (req,res) => {
+    res.sendFile('streaming.html', { root: __dirname + '/views' });
+});
+
 router.get('/play',(req,res) => {
     if(client.get('username')) {
         res.sendFile('play.html', { root: __dirname + '/views' });
@@ -61,6 +72,69 @@ router.get('/logout',(req,res) => {
 
 app.use('/', router);
 
-app.listen(process.env.PORT || 3000,() => {
+/*app.listen(process.env.PORT || 3000,() => {
     console.log(`App Started on PORT ${process.env.PORT || 3000}`);
-});
+});*/
+server.listen(3000);
+
+// aki empieza la mierda
+
+var numUsers = 0;
+
+io.on('connection', (socket) => {
+    var addedUser = false;
+  
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', (data) => {
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    });
+  
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', (username) => {
+      if (addedUser) return;
+  
+      // we store the username in the socket session for this client
+      socket.username = username;
+      ++numUsers;
+      addedUser = true;
+      socket.emit('login', {
+        numUsers: numUsers
+      });
+      // echo globally (all clients) that a person has connected
+      socket.broadcast.emit('user joined', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    });
+  
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      });
+    });
+  
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      });
+    });
+  
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+      if (addedUser) {
+        --numUsers;
+  
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+      }
+    });
+  });
